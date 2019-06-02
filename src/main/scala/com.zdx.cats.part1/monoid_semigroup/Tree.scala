4aa44b8a02d487cad5b44cbf91e4c9f55ed87882
zdx1989 <18565653302@163.com>
2019-06-02 11:36:56 +0800

    functor of Tree

---
+++ src/main/scala/com.zdx.cats.part1/monoid_semigroup/Tree.scala
@@ -0,0 +1,27 @@
+package com.zdx.cats.part1.monoid_semigroup
+
+import cats.Functor
+
+/**
+  * Created by zhoudunxiong on 2019/6/2.
+  */
+trait Tree[+A]
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+case class Leaf[A](value: A) extends Tree[A]
+
+object Tree {
+
+  def leaf[A](value: A): Tree[A] = Leaf(value)
+
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+
+  implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
+
+    def map[A, B](fa: Tree[A])(f: (A) => B): Tree[B] = fa match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    }
+  }
+}
