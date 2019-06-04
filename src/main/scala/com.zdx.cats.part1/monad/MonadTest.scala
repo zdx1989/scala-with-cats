d682c3dc72235835e8909f64e0dfb8f9e36ade6a
zdx1989 <18565653302@163.com>
2019-06-04 11:44:43 +0800

    Eval defer make code stack safe

--- src/main/scala/com.zdx.cats.part1/monad/MonadTest.scala
+++ src/main/scala/com.zdx.cats.part1/monad/MonadTest.scala
@@ -9,6 +9,7 @@ object MonadTest {
 
   import scala.language.higherKinds
   import cats.Monad
+  import cats.Eval
   import cats.syntax.flatMap._
   import cats.syntax.functor._
   import cats.syntax.either._
@@ -28,4 +29,31 @@ object MonadTest {
       else
         "negative stoppping".asLeft[Int]
     }
+
+  val saying = Eval.always {
+    println("Step 1"); "a cat"
+  }.map { str =>
+    println("Step 2"); s"$str sat on"
+  }.memoize.map { str =>
+    println("Step 3"); s"$str the mat"
+  }
+
+  def factorial(n: BigInt): Eval[BigInt] =
+    if (n == 1) {
+      Eval.now(n)
+    } else {
+      Eval.defer(factorial(n - 1).map(_ * n))
+    }
+
+  def foldRightEval[A, B](list: List[A], acc: Eval[B])(func: (A, Eval[B]) => Eval[B]): Eval[B] = {
+    list match {
+      case Nil => acc
+      case head :: tail => Eval.defer(func(head, foldRightEval(tail, acc)(func)))
+    }
+  }
+
+  def foldRight[A, B](list: List[A], acc: B)(func: (A, B) => B): B =
+    foldRightEval(list, Eval.now(acc)) { (a, eb) =>
+      eb.map(b => func(a, b))
+    }.value
 }
