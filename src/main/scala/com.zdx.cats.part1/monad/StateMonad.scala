25653ee3993fd21e969d997452592801d6bac83c
zdx1989 <18565653302@163.com>
2019-06-05 20:33:11 +0800

    calculator example

--- src/main/scala/com.zdx.cats.part1/monad/StateMonad.scala
+++ src/main/scala/com.zdx.cats.part1/monad/StateMonad.scala
@@ -74,6 +74,54 @@ object StateMonad {
 
   val (state2, result2) = forDemo.run(1).value
 
-  //
+  import cats.data.State
 
+  type CalcState[A] = State[List[Int], A]
+
+  def evalOne(sym: String): CalcState[Int] = {
+    sym match {
+      case "+" => operate(_ + _)
+      case "_" => operate(_ - _)
+      case "*" => operate(_ * _)
+      case "/" => operate(_ / _)
+      case num => operand(num.toInt)
+    }
+  }
+
+  def operand(num: Int): CalcState[Int] = State[List[Int], Int] { state =>
+    (num :: state, num)
+  }
+
+  def operate(func: (Int, Int) => Int): CalcState[Int] = State[List[Int], Int] {
+    case b :: a :: tail =>
+      val ans = func(a, b)
+      (ans :: tail, ans)
+    case _ =>
+      sys.error("Fail!")
+  }
+
+  val program = for {
+    _ <- evalOne("1")
+    _ <- evalOne("2")
+    ans <- evalOne("+")
+  } yield ans
+
+  program.run(Nil).value
+
+
+  import cats.syntax.applicative._
+  def evalAll(input: List[String]): CalcState[Int] = {
+    input.foldLeft(0.pure[CalcState]){ (a, b) =>
+      a.flatMap(_ => evalOne(b))
+    }
+  }
+
+  val program1 = for {
+    _ <- evalAll(List("1", "2", "+"))
+    _ <- evalAll(List("3", "4", "+"))
+    ans <- evalOne("*")
+  } yield ans
+
+  def evalInput(input: String): Int =
+    evalAll(input.split(" ").toList).runA(Nil).value
 }
